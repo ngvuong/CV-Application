4c0bfe879f216de22f0e4b97092872f40349faa9
ngvuong <vminguyen@ucdavis.edu>
2021-11-07 03:04:12 -0800

    update App component to use cvform

--- src/CVApp.js
+++ src/CVApp.js
@@ -1,41 +1,10 @@
 import React from "react";
-import Personal from "./components/Personal";
 import Education from "./components/Education";
 import Experience from "./components/Experience";
+import CVForm from "./CVForm";
 import "./styles/App.css";
 
 class CVApp extends React.Component {
-  constructor() {
-    super();
-    this.handleAddEducationField = this.handleAddEducationField.bind(this);
-    this.handleRemoveEducationField =
-      this.handleRemoveEducationField.bind(this);
-    this.handleAddExperienceField = this.handleAddExperienceField.bind(this);
-    this.handleRemoveExperienceField =
-      this.handleRemoveExperienceField.bind(this);
-    this.state = { educationFieldNum: 1, experienceFieldNum: 1 };
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  handleAddEducationField() {
-    this.setState({ educationFieldNum: this.state.educationFieldNum + 1 });
-  }
-
-  handleRemoveEducationField() {
-    this.setState({ educationFieldNum: this.state.educationFieldNum - 1 });
-  }
-
-  handleAddExperienceField() {
-    this.setState({ experienceFieldNum: this.state.experienceFieldNum + 1 });
-  }
-
-  handleRemoveExperienceField() {
-    this.setState({ experienceFieldNum: this.state.experienceFieldNum - 1 });
-  }
-
   render() {
     const additionalEducationFields = [];
 
@@ -55,26 +24,7 @@ class CVApp extends React.Component {
 
     return (
       <div className="App">
-        <form className="CvForm" onSubmit={this.handleSubmit}>
-          <Personal />
-          <Education heading="Education" />
-          {additionalEducationFields}
-          <button
-            className="Btn AddFieldsBtn"
-            onClick={this.handleAddEducationField}
-          >
-            {"\u2795"}
-          </button>
-          <Experience heading="Experience" />
-          {additionalExperienceFields}
-          <button
-            className="Btn AddFieldsBtn"
-            onClick={this.handleAddExperienceField}
-          >
-            {"\u2795"}
-          </button>
-          <button className="Btn SaveBtn">Save</button>
-        </form>
+        <CVForm />
       </div>
     );
   }
